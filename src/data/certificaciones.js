07730b99f4716c86aaf00f6f22bd79c8f327501f
JPicazo <joel.picazo@cinvestav.mx>
2025-06-08 13:47:42 -0600

    Correccion de Certificaciones

--- src/data/certificaciones.js
+++ src/data/certificaciones.js
@@ -3,7 +3,7 @@ export const certificaciones = [
     nombre: "Google Project Management",
     institucion: "Institution: Google/Coursera",
     estado: "Completed",
-    link: "https://coursera.org/verify/XYZ123"
+    link: "https://www.coursera.org/account/accomplishments/professional-cert/certificate/W528PANANA86"
   },
   {
     nombre: "Google Data Analytics",
