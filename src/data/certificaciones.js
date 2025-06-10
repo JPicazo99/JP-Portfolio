37f20ec6a90856f58cbfc58906e2a6f2b2875318
JPicazo <joel.picazo@cinvestav.mx>
2025-06-10 11:25:40 -0600

    Add Certification Nand to Tetris

--- src/data/certificaciones.js
+++ src/data/certificaciones.js
@@ -5,6 +5,14 @@ export const certificaciones = [
     estado: "Completed",
     link: "https://www.coursera.org/account/accomplishments/professional-cert/certificate/W528PANANA86"
   },
+
+  {
+    nombre: "Build a Modern Computer from First Principles: From Nand to Tetris (Project-Centered Course)",
+    institucion: "Hebrew University of Jerusalem",
+    estado: "Completed",
+    link: "https://www.coursera.org/account/accomplishments/professional-cert/certificate/W528PANANA86"
+  },
+
   {
     nombre: "Google Data Analytics",
     institucion: "Institution: Google/Coursera",
